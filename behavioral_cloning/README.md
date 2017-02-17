ed093d6647c8871a1e464a5da9cb847c25fa1290
dzorlu <deniz@monahq.com>
2017-02-16 21:26:31 -0500

    intermediary results

--- behavioral_cloning/README.md
+++ behavioral_cloning/README.md
@@ -1,15 +1,17 @@
-
+#Behavioral Cloning
 
 The goal of the exercise is to get a car to drive autonomously in a simulated setting. Udacity provides the simulator, which includes two tracks that the model can be trained on. First track is easier. Second track includes hills, tight turns, shadows on the road, bridges etc, which makes it harder to navigate.
 
 Although Udacity provides dataset to get something working off the bat, I quickly realized that I not only need to collect more data through the simulator, but also augment and preprocess the data in smart ways to keep the car on the track and out of harms way.
 
 
-The Model
+##The Model
 
 There is nothing novel about the model I have used. The model normalizes the input layer akin to that in NVIDIA model. It is a standard CNN that has three convolutional layers, each of which followed by 2x2 max pooling. Convolutional layers are followed by three fully connected layers. Because the fully connected layers have a large capacity, L2 norm is used to battle overfitting. Activation layers are all RELU layers. I used Adam optimizer, which attempts to apply best of both worlds - momentum and varying learning rates for each parameter. Because the prediction is the steering angle of the car, the error rate is defined as mean squared error.
 
-Data Generation
+The code details can be found under `model` folder.
+
+##Data Generation
 
 The real meat of the project lies in data generation. In training mode, the simulator produces three images per each frame corresponding to right-, left-, and center-mounted cameras. Inclusion of right and left cameras is common trick used to teach the car to swirl back to center the road. In order to teach the model to swirl back to the center of the road, the recorded steering angle is adjusted appropriately. Additionally, I recorded extra data to provide data instances when the car is close to going off the road. By providing 'recovery' instances, we try to capture data on the tails of the steering angle distribution.
 
@@ -19,4 +21,6 @@ The second track is particularly hilly. The hilliness is manifested in the image
 
 The data generator closely resembles that of Keras. The reason why I built a new one instead of expanding the class is because Keras generator does not seem to augment the target variables along with feature variables.
 
-In all, the training data has close to 50,000 images including left and right camera angles. 
+In all, the training data has close to 50,000 images including left and right camera angles.
+
+The code details can be found under `data_generator` folder.
--- behavioral_cloning/data_generator/image.py
+++ behavioral_cloning/data_generator/image.py
@@ -5,6 +5,9 @@ import cv2
 import numpy as np
 import threading
 import os
+import matplotlib.pyplot as plt
+
+DEBUG = True
 
 def _random_flip(x, y):
     if np.random.random() < 0.5:
@@ -19,12 +22,22 @@ def _random_brightness(img):
     image1 = cv2.cvtColor(image1,cv2.COLOR_HSV2RGB)
     return image1
 
-def _crop_image(img, output_shape):
-    return cv2.resize(img[40:140,:,:], (output_shape[1], output_shape[0]))
+def _crop_image(x, output_shape):
+    return cv2.resize(x[40:140,:,:], (output_shape[1], output_shape[0]))
+
+def _mean_substract(x, rescale):
+    return x / rescale - 0.5
+
+def _preprocess(x, rescale, output_shape):
+    #print(x.shape)
+    x = _crop_image(x, output_shape)
+    #x = _mean_substract(x, rescale)
+    return x
 
 def _file_to_image(root_path, fname):
     _loc = os.path.join(root_path, fname)
     return cv2.imread(_loc)
+
 
 class ImageDataGenerator(object):
     """
@@ -35,18 +48,21 @@ class ImageDataGenerator(object):
     Generate minibatches of training data.
     Acts like a DirectoryIterator
     """
-    def __init__(self, target_map, input_shape, output_shape, batch_size, root_path, shuffle=True):
+    def __init__(self, target_map, input_shape, output_shape, batch_size, root_path, shuffle=True, training=True):
         self.target_map= target_map
         self.output_shape = output_shape
         self.input_shape = input_shape
         self.batch_size = batch_size
         self.rescale = 1./255
+        self.shift_degree = 22.5
         self.lock = threading.Lock()
         self.root_path = root_path
         self.filenames = os.listdir(root_path)
         self.batch_index = 0
         self.N = len(self.target_map.keys())
         self.index_generator = self._flow_index(self.N, batch_size, shuffle)
+        self.y_hist = []
+        self.training = training
 
     def reset(self):
         self.batch_index = 0
@@ -74,14 +90,18 @@ class ImageDataGenerator(object):
         with self.lock:
             ix_array, current_index, current_batch_size = next(self.index_generator)
         batch_x = np.zeros([current_batch_size] + list(self.output_shape))
-        print(batch_x.shape)
         batch_y = np.zeros(current_batch_size)
         for i, j in enumerate(ix_array):
             fname = self.filenames[j]
             x = self.file_to_image(fname)
             y = self.target_map[fname]
-            x, y = self.transform(x, y)
-            x = self.mean_substract(x)
+
+            if self.training:
+                # Flip LR + perspective transformation
+                x, y = self.transform(x, y)
+            # Preprocess
+            x = self.preprocess(x)
+
             batch_x[i,:,:] = x
             batch_y[i] = y
         return batch_x, batch_y
@@ -93,9 +113,9 @@ class ImageDataGenerator(object):
         return _file_to_image(self.root_path, fname)
 
     def transform(self, x, y):
-        x = self.crop_image(x)
         x,y = self.random_flip(x, y)
         x = self.random_brightness(x)
+        x = self.random_vertical_shift(x)
         return x, y
 
     def random_flip(self, x, y):
@@ -104,8 +124,22 @@ class ImageDataGenerator(object):
     def random_brightness(self, x):
         return _random_brightness(x)
 
+    def random_vertical_shift(self, x):
+        h,w,_ = x.shape
+        horizon = 2*h/5
+        v_shift = np.random.randint(-h//8,h//8)
+        # from
+        pts1 = np.float32([[0,horizon],[w, horizon],[0,h],[w,h]])
+        # to
+        pts2 = np.float32([[0,horizon+v_shift],[w,horizon+v_shift],[0,h],[w,h]])
+        M = cv2.getPerspectiveTransform(pts1,pts2)
+        return cv2.warpPerspective(x, M, (w,h), borderMode=cv2.BORDER_REPLICATE)
+
+    def preprocess(self, x):
+        return _preprocess(x, self.rescale, self.output_shape)
+
     def crop_image(self, x):
         return _crop_image(x, self.output_shape)
 
     def mean_substract(self, x):
-        return  x / self.rescale - 0.5
+        return _mean_substract(x, self.rescale)
